f3e24f9d23ac44b0b7895a2d9ecb04616e1ebedc
student <o1073008@gm.pu.edu.tw>
2020-05-13 12:02:10 +0800

    第一次 Commit

---
+++ app/src/main/java/o1073008/pu/edu/tw/hellowworld/MainActivity.java
@@ -0,0 +1,13 @@
+package o1073008.pu.edu.tw.hellowworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

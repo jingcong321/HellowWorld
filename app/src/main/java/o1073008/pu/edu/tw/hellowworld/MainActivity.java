1662eafdd2f3470cf1dd4737e4fc6914bd7a8d10
student <o1073008@gm.pu.edu.tw>
2020-05-13 12:05:01 +0800

    第二次 修改文件

--- app/src/main/java/o1073008/pu/edu/tw/hellowworld/MainActivity.java
+++ app/src/main/java/o1073008/pu/edu/tw/hellowworld/MainActivity.java
@@ -2,6 +2,7 @@ package o1073008.pu.edu.tw.hellowworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txv = (TextView) findViewById(R.id.txv);
+        txv.setText("Hello student");
+
     }
+
 }
